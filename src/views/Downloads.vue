7aa06641cf6d9d456159ad35088fca38bf1591a3
losowyLP <arandomone.businnes@gmail.com>
2023-04-12 03:52:10 +0200

    do whatever this is bcs the downloads pagr looked a lil funky before

--- src/views/Downloads.vue
+++ src/views/Downloads.vue
@@ -6,13 +6,13 @@ import Page from "../templates/page.vue";
   <Page title="downloads">
     <div class="flex justify-center">
       <div
-        class="m-2 flex flex-col md:flex-row justify-around gap-32 md:w-6/12"
+        class="m-2 flex flex-col md:flex-row justify-around gap-32 lg:w-6/12"
       >
         <router-link to="/downloads/launchpad"
-          ><img src="../assets/images/launchpad.png" class="h-64"
+          ><img src="../assets/images/launchpad.png" class="h-64 w-64"
         /></router-link>
         <a href="https://github.com/losowyLP" target="_blank"
-          ><img src="../assets/images/software.png" class="h-64"
+          ><img src="../assets/images/software.png" class="h-64 w-64"
         /></a>
       </div>
     </div>
