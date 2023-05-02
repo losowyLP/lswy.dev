3d2e66b56e1570ef7665afe3c1dc772ebc516361
losowyLP <arandomone.businnes@gmail.com>
2023-05-02 18:31:18 +0200

    idfk what is happening

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -27,17 +27,10 @@ export default {
 </script>
 
 <template>
-  HEAD
-  <div class="text-slate-200 h-12 flex justify-center">
-    <hr class="fixed w-64" />
-    <h1 class="relative top-1/4">made with ❤️ by lswy</h1>
-  </div>
-  =======
   <footer class="fixed bottom-2 min-w-full">
     <div class="text-slate-200 h-12 flex justify-center">
       <hr class="absolute w-64" />
       <h1 class="relative top-1/4">made with {{ heart }} by lswy</h1>
     </div>
   </footer>
-  >>>>>>> 9c2db3c (add a random heart in the footer of the site)
 </template>
