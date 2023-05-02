2a7a9357d25a46eda1c8ec4a0e03962914714d1a
losowyLP <arandomone.businnes@gmail.com>
2023-05-02 18:27:55 +0200

    add a random heart in the footer of the site

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,43 @@
+<script>
+const hearts = [
+  "🩷",
+  "❤️",
+  "🧡",
+  "💛",
+  "💚",
+  "🩵",
+  "💙",
+  "💜",
+  "🖤",
+  "🩶",
+  "🤍",
+  "🤎",
+  "💖",
+];
+
+const randHeart = Math.floor(Math.random() * hearts.length);
+
+export default {
+  data() {
+    return {
+      heart: hearts[randHeart],
+    };
+  },
+};
+</script>
+
 <template>
+  HEAD
   <div class="text-slate-200 h-12 flex justify-center">
-	  <hr class="fixed w-64" />
-	  <h1 class="relative top-1/4">made with ❤️ by lswy</h1>
+    <hr class="fixed w-64" />
+    <h1 class="relative top-1/4">made with ❤️ by lswy</h1>
   </div>
+  =======
+  <footer class="fixed bottom-2 min-w-full">
+    <div class="text-slate-200 h-12 flex justify-center">
+      <hr class="absolute w-64" />
+      <h1 class="relative top-1/4">made with {{ heart }} by lswy</h1>
+    </div>
+  </footer>
+  >>>>>>> 9c2db3c (add a random heart in the footer of the site)
 </template>
