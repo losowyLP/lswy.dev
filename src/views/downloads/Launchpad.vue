e04bd8361c7c2c63784755a77ac7f91bf373440c
lswy <arandomone.businnes@gmail.com>
2023-05-21 14:57:03 +0200

    get rid of the blur on the launchpad projects download page

--- src/views/downloads/Launchpad.vue
+++ src/views/downloads/Launchpad.vue
@@ -4,19 +4,18 @@ import Page from "../../templates/page.vue";
 
 <template>
   <Page title="Launchpad Projects">
-    <h3 class="opacity-80">blurred = unavailable</h3>
     <div class="flex justify-center">
       <div class="w-8/12 m-2 grid lg:grid-cols-3 gap-2 justify-items-center">
-        <div class="">
+        <div class="cursor-not-allowed">
           <img
             src="../../assets/images/thumbnails/meantforyou.png"
-            class="pb-1 blur ring ring-indigo-500"
+            class="ring ring-indigo-500"
           /><span>OMFG - Meant for You</span>
         </div>
-        <div>
+        <div class="cursor-not-allowed">
           <img
             src="../../assets/images/thumbnails/fire.png"
-            class="pb-1 blur ring ring-sky-500"
+            class="ring ring-sky-500"
           />
           <span>Brook Xiao - Fire (ft. Rachel Horter)</span>
         </div>
